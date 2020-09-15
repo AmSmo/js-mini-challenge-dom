3fe943da26ec6e3af440c848eb655929fcf1d148
AmSmo <adam.smolenski@gmail.com>
2020-09-15 17:45:00 -0400

    done

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
-console.log("Here's your header:", header)
-
+const header = document.querySelector("#header");
+console.log("Here's your header:", header);
+header.style.color = "red";
 
 /***** Deliverable 2 *****/
+const playerContainer = document.querySelector(".player-container")
+console.log(playerContainer)
+PLAYERS.forEach(player => {
+    const playerDiv = document.createElement('div')
+    playerDiv.className="player"
+    playerDiv.setAttribute("data-number", `${player.number}`)
+    const newPlayer = `
+        <h3>
+            ${player.name} (<em>${player.nickname}</em>)
+  </h3>
+        <img src="${player.photo}" alt="${player.name}">
+`
 
+    playerDiv.innerHTML = newPlayer
+    playerContainer.appendChild(playerDiv)
+});
 
 /***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
+console.log("PLAYERS array looks like this:", PLAYERS)
 
 
 /***** Deliverable 4 *****/
+
+document.querySelector("[data-number='7']").remove()
